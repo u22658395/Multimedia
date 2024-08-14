9b0460c9aa5f11b10bfe42e9234ada0e534f54b0
u22658395 <u22658395@tuks.co.za>
2024-08-14 10:15:10 +0200

    Implemented the ListPets function

--- Assignments/Assignment 1/script.js
+++ Assignments/Assignment 1/script.js
@@ -21,9 +21,45 @@ class PetHandler {
   }
 
   listAdoptedPetsByDate(){
-   
     return pets.filter(pet => pet.adopted === true)
-        .sort((a, b) => new Date(b.adoptedDate) - new Date(a.adoptedDate)); // Sort by adoption date in reverse chronological order
-    
+      .sort((a, b) => new Date(b.adoptedDate) - new Date(a.adoptedDate));
+  }
+
+  ListPets(){
+    function createPetItem(pet) {
+      let petString = `${pet.name} | ${pet.species} | Age: ${pet.age}`;
+      if (pet.adopted) {
+        petString += " | Adopted!";
+      }
+      return petString;
+    }
+  
+    let petsToReturn;
+  
+    if (arguments.length === 0) {      
+      // No arguments provided, use the member variable
+      petsToReturn = this.pets;
+    }
+    else {
+      if(arguments.length === 1 && Array.isArray(arguments[0])) {
+        // Single array argument provided
+        petsToReturn = arguments[0];
+      } 
+      else {
+        // Multiple object arguments provided
+        petsToReturn = Array.prototype.slice.apply(arguments);
+      }
+
+    }
+  
+    return petsToReturn.map(createPetItem);
   }
 }
+
+var pHandler = new PetHandler(pets);
+console.log("findPetsInAgeRange: ", pHandler.findPetsInAgeRange(1,5));
+console.log("listAdoptedPetsByDate", pHandler.listAdoptedPetsByDate());
+console.log("ListPets: ", pHandler.ListPets({ name: "Buddy", species: "dog", age: 10, adopted: true, adoptedDate: "2021-02-01", adoptionFee: 735 },{ name: "Polly", species: "bird", age: 1, adopted: false, adoptedDate: "", adoptionFee: 560 }));
+console.log("calculateUniqueAdoptionFee: ", pHandler.calculateUniqueAdoptionFee("Buddy",  "Coco",  "10",  "true",  "Buddy",  "735"));
+// console.log(pHandler.ListPets());
+
