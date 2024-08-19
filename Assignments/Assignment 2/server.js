2997a3610c111e83e8519e5ee2fe7f5438a135f2
u22658395 <u22658395@tuks.co.za>
2024-08-19 11:35:03 +0200

    Created an Express server in Server.js

---
+++ Assignments/Assignment 2/server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000;
+
+app.use(express.static())
+app.get('/', (req, res) =>{
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`)
+})
