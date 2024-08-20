0f204ee1d89b1005544f14ee787e842979737835
u22658395 <u22658395@tuks.co.za>
2024-08-20 10:44:53 +0200

    Implemented the form's Submit event

--- Assignments/Assignment 2/index.js
+++ Assignments/Assignment 2/index.js
@@ -1,5 +1,23 @@
 const socket = io();
+var form = document.getElementById("poll");
 
 socket.on("connect",()=>{
     console.log(`I connected with ID: ${socket.id}`);
+    
+    form.addEventListener("submit",formEvent)
 })
+
+
+function formEvent(event) {
+    event.preventDefault();
+
+    var selectedInput = document.querySelector('input[name="catName"]:checked');
+
+    var votes = [];
+    if(selectedInput !== null){
+        var catName = selectedInput.value;
+        votes[catName]++;        
+        socket.emit("vote", catName);
+
+    }
+}
