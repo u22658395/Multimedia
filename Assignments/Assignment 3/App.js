a6f6644a047c101ca6d6df0eb662551faa9aef74
u22658395 <u22658395@tuks.co.za>
2024-08-28 11:37:08 +0200

    Moved the components to App.js

--- Assignments/Assignment 3/App.js
+++ Assignments/Assignment 3/App.js
@@ -62,9 +62,41 @@ const events = [
 ];
 
 // Event Component
-
-
+class Event extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <h1>{this.props.name}</h1>
+        <h6>{this.props.date}</h6>
+        <p>{this.props.description}</p>  
+      </div>
+    )
+  }
+}      
+      
+      
 // EventFeed Component
+class EventFeed extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <>
+        {
+          this.props.events.sort((a,b)=>{
+            new Date(b.date) - new Date(a.date)
+          }).map(event=>{
+            return <Event name={event.name} date={event.date} description={event.description}/>
+          })
+        }
+      </>
+    )
+  }
+}
 
 // Search Component
 
@@ -73,4 +105,5 @@ const events = [
 
 // render here
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<EventFeed events={events}/>);
 // complete this code
