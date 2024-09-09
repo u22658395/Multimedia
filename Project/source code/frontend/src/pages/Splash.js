3d4918b3fbe86449d067faf1ee0db8704b88fcbf
u22658395 <u22658395@tuks.co.za>
2024-09-09 10:55:02 +0200

    Created the Splash component

--- Project/source code/frontend/src/pages/Splash.js
+++ Project/source code/frontend/src/pages/Splash.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-class Splash extends React.Component {
-    constructor(props) {
-        
-    }
+export class Splash extends React.Component {
     render(){
         return(
-            <
+            <>
+                <h1 class="welcome-text" id="heading-text">Welcome to GrooveList: Your Musical Oasis</h1>
+                <p class="welcome-text" id="sub-heading">Step into GrooveList, where your musical journey begins. Here, melodies come alive, playlists tell stories, and every
+                beat connects us. Discover, create, and share playlists that resonate with your soul. Whether you're a passionate
+                curator, an avid listener, or a musical explorer, GrooveList is the perfect place for you. Connect with fellow music
+                lovers, uncover hidden gems, and let the rhythm guide you.</p>
+
+
+                <div id="slider">
+                    
+                    
+                </div>
+
+            </>
         )
     }
 }
