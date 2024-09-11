1b0f7635c4cfea94d59dd30e2cd2a9e7f79c18d2
u22658395 <u22658395@tuks.co.za>
2024-09-11 10:07:35 +0200

    Updated the page names in the navbar

--- Project/source code/frontend/src/components/Navbar.js
+++ Project/source code/frontend/src/components/Navbar.js
@@ -32,7 +32,7 @@ export class Navbar extends React.Component {
 
                 <span id="pages">
                     <Link to="/" className="page">Home</Link>
-                    <Link to="/" className="page">Playlist</Link>
+                    <Link to="/" className="page">Playlists</Link>
                     <Link to="/" className="page">Profile</Link>
                 </span>
 
