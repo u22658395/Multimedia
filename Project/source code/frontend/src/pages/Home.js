b248a805e633f2b75fceb29595b24b6615524bf6
u22658395 <u22658395@tuks.co.za>
2024-09-13 01:33:25 +0200

    Implemented the Home page heading

--- Project/source code/frontend/src/pages/Home.js
+++ Project/source code/frontend/src/pages/Home.js
@@ -4,6 +4,13 @@ export class Home extends React.Component {
         return (
             <div id="content">
                 <h1>Home</h1>
+
+                <div id="home-header">
+                    <div id="feed-headings-container">
+                        <span className="feed-heading">Playlists</span>
+                        <span className="feed-heading">Songs</span>
+                    </div>
+                </div>
             </div>
         );
     }
