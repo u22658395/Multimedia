0d01ef5a220cee9463886d51c65debc735b126fb
u22658395 <u22658395@tuks.co.za>
2024-09-12 23:19:31 +0200

    Implemented the render method of SongFeed

--- Project/source code/frontend/src/components/SongFeed.js
+++ Project/source code/frontend/src/components/SongFeed.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Song } from './Song';
 
 export class Songs extends React.Component {
     constructor(props) {
         super(props)
+        this.songs = this.props.songs;
     }
     render() {
         return(
-            
+            <div id='songs'>
+
+                {
+                    this.songs.map(song=>{
+                        <Song title={song.title} artist={song.artist} album={song.album} duration={song.duration} link={song.link} />
+                    })
+                }
+                
+            </div>
         )
     }
 }
