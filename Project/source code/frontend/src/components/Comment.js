5137472934dc7d70c85b72bd3d3afb20a58b0f19
u22658395 <u22658395@tuks.co.za>
2024-09-13 01:17:22 +0200

    renamed comment to CommentFeed

--- Project/source code/frontend/src/components/Comment.js
+++ Project/source code/frontend/src/components/Comment.js
@@ -5,16 +5,18 @@ export class Comment extends React.Component {
         super(props)
     }
     render() {
-        < div key={this.props.comment.id} className='comment' >
-            <div className='comment-username'>{this.props.comment.username}</div>
-            <div className='comment-body'>
-                <span className='comment-text'>
-                    {this.props.comment.body}
-                </span>
-                <span className='comment-image-container'>
-                    <img className="comment-image" src={this.props.comment.image} />
-                </span>
+        return(
+            < div className='comment' >
+                <div className='comment-username'>{this.props.comment.username}</div>
+                <div className='comment-body'>
+                    <span className='comment-text'>
+                        {this.props.comment.body}
+                    </span>
+                    <span className='comment-image-container'>
+                        <img className="comment-image" src={this.props.comment.image} />
+                    </span>
+                </div>
             </div>
-        </div>
+        )
     }
 }
