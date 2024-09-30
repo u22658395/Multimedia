faf10ed487ee8d9a2979008e948dd5dd6363ca0f
u22658395 <u22658395@tuks.co.za>
2024-09-30 09:27:37 +0200

    allowed a click on a song to diret to the link of the song

--- Project/source code/frontend/src/components/Song.js
+++ Project/source code/frontend/src/components/Song.js
@@ -6,7 +6,7 @@ export class Song extends React.Component{
     }
     render(){
         return(
-            <a src={this.props.link} className='song'>
+            <a href={this.props.link} className='song'>
                 <span className='song-property song-title'>{this.props.title}</span>
                 <span className='song-property song-artist'>{this.props.artist}</span>
                 <span className='song-property song-album'>{this.props.album}</span>
